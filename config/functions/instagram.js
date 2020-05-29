688e68dab2fdb901fce7b12efad559e84c8e281f
vrehm <vincentrehm@yahoo.fr>
2020-05-29 21:27:01 +0200

    cleaner ig function

--- config/functions/instagram.js
+++ config/functions/instagram.js
@@ -17,97 +17,23 @@ module.exports = async() => {
 
     for (let index = 0; index < data.length; index++) {
         const post = data[index]
+        const search = await strapi.query('instagram-posts').findOne({ ig_id_contains: post.id });
 
-        await strapi.query('instagram-posts').create({
-            ig_id: post.id,
-            caption: post.caption,
-            media_type: post.media_type,
-            media_url: post.media_url
-        })
+        if (search === null) {
+            await strapi.query('instagram-posts').create({
+                ig_id: post.id,
+                caption: post.caption,
+                media_type: post.media_type,
+                media_url: post.media_url
+            })
+        } else {
+            await strapi.query('instagram-posts').update({ ig_id: post.id }, {
+                caption: post.caption,
+                media_type: post.media_type,
+                media_url: post.media_url
+            })
+        }
     }
 
-    console.log('Import is done')
-
-    // await strapi.query('instagram-posts').create({
-    //     ig_id: data[0].id,
-    //     caption: data[0].caption,
-    //     media_type: data[0].media_type,
-    //     media_url: data[0].media_url
-    // })
-
-    // .then((response) => {
-    //     loading = false
-
-    //     if (response.status === 400) error = response.error.message
-
-    //     if (response.status === 200) {
-    //         for (const n in response.data.data) {
-    //             if (mediatypes.includes(response.data.data[n].media_type)) {
-    //                 if (feeds.length >= count) {
-    //                     return
-    //                 }
-
-    //                 const data = response.data.data[n]
-
-    //                 if (data.children) {
-    //                     // feeds.push({ id: 'reserved' })
-    //                     // const caption = data.caption
-    //                     // const children = data.children.data
-    //                     // delete data.children
-    //                     // for (let index = 0; index < children.length; index++) {
-    //                     //     const child = children[index].id
-    //                     //     const endpoint = 'https://graph.instagram.com/' + child
-
-    //                     //     await axios
-    //                     //         .get(endpoint, {
-    //                     //             params: {
-    //                     //                 access_token: IG_TOKEN,
-    //                     //                 fields: 'media_url,media_type'
-    //                     //             }
-    //                     //         })
-    //                     //         .then((response) => {
-    //                     //             const igChild = response.data
-    //                     //             if (igChild.media_type === 'IMAGE') {
-    //                     //                 igChild.caption = caption
-    //                     //                 feeds.push(igChild)
-    //                     //             }
-    //                     //         })
-    //                     //         .catch((error) => {
-    //                     //             throw error
-    //                     //         })
-    //                     // }
-    //                 } else {
-    //                     feeds.push(data)
-    //                         // const create = strapi.query('instagram-posts').create({
-    //                         //     ig_id: data.id,
-    //                         //     caption: 'toto',
-    //                         //     media_type: 'toto',
-    //                         //     media_url: 'toto'
-    //                         // })
-    //                     console.log(data)
-    //                 }
-    //             }
-    //         }
-    //     }
-    // })
-    // .catch((error) => {
-    //     throw error
-    // })
-
-    // for (let index = 0; index < data.length; index++) {
-    //     const post = data[index];
-    //     console.log(post);
-
-    //     // await strapi.query('instagram-posts').create({
-    //     //         ig_id: post.id,
-    //     //         caption: 'toto',
-    //     //         media_type: post.media_type,
-    //     //         media_url: post.media_url
-    //     //     })
-    //     //     .catch((error) => {
-    //     //         throw error
-    //     //     });
-    // }
-    // console.log(test);
-    // console.log(first, create, data)
+    console.log('Import for last 25 Instagram posts is done')
 }
