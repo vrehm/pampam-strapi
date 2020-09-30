8b547cc21ae1880854d4d0a87c3b45b016f57e07
vrehm <vincentrehm@yahoo.fr>
2020-09-30 18:52:07 +0200

    fix incorrect keys

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
             connector: 'bookshelf',
             settings: {
                 client: 'postgres',
-                host: env('POSTGRESQL_ADDON_HOST', '127.0.0.1'),
-                port: env.int('POSTGRESQL_ADDON_PORT', 5432),
-                database: env('POSTGRESQL_ADDON_NAME', 'pampam-strapi'),
+                host: '127.0.0.1',
+                port: 5432,
+                database: 'pampam-strapi',
                 username: env('DEV_USER'),
                 password: env('DEV_PASSWORD'),
-                ssl: env.bool('POSTGRESQL_ADDON_SSL', false),
+                ssl: false,
             }
         },
     },
