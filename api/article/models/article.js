af3c819602d42f2118c17a129f5c365531d7076e
vrehm <vincentrehm@yahoo.fr>
2020-09-30 18:18:35 +0200

    working version of strapi 3.1.6 in local

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -7,57 +7,24 @@ const slugify = require('slugify');
  */
 
 module.exports = {
-    // Before saving a value.
-    // Fired before an `insert` or `update` query.
-    beforeSave: async(model, attrs, options) => {
-        if (options.method === 'insert' && attrs.title) {
-            model.set('slug', slugify(attrs.title, { lower: true }));
-        } else if (options.method === 'update' && attrs.title) {
-            attrs.slug = slugify(attrs.title, { lower: true });
-        }
+    lifecycles: {
+        async beforeCreate(data) {
+            if (data.title) {
+                data.slug = slugify(data.title, { lower: true });
+            }
+
+            const today = new Date();
+            data.published_at = today
+        },
+        async beforeUpdate(params, data) {
+            if (data.title) {
+                data.slug = slugify(data.title, { lower: true });
+            }
+
+            if (!data.published_at) {
+                const today = new Date();
+                data.published_at = today
+            }
+        },
     },
-
-    // After saving a value.
-    // Fired after an `insert` or `update` query.
-    // afterSave: async (model, response, options) => {},
-
-    // Before fetching a value.
-    // Fired before a `fetch` operation.
-    // beforeFetch: async (model, columns, options) => {},
-
-    // After fetching a value.
-    // Fired after a `fetch` operation.
-    // afterFetch: async (model, response, options) => {},
-
-    // Before fetching all values.
-    // Fired before a `fetchAll` operation.
-    // beforeFetchAll: async (model, columns, options) => {},
-
-    // After fetching all values.
-    // Fired after a `fetchAll` operation.
-    // afterFetchAll: async (model, response, options) => {},
-
-    // Before creating a value.
-    // Fired before an `insert` query.
-    // beforeCreate: async (model, attrs, options) => {},
-
-    // After creating a value.
-    // Fired after an `insert` query.
-    // afterCreate: async (model, attrs, options) => {},
-
-    // Before updating a value.
-    // Fired before an `update` query.
-    // beforeUpdate: async (model, attrs, options) => {},
-
-    // After updating a value.
-    // Fired after an `update` query.
-    // afterUpdate: async (model, attrs, options) => {},
-
-    // Before destroying a value.
-    // Fired before a `delete` query.
-    // beforeDestroy: async (model, attrs, options) => {},
-
-    // After destroying a value.
-    // Fired after a `delete` query.
-    // afterDestroy: async (model, attrs, options) => {}
 };
